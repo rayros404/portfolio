2426af5f31209ae0685f9e8ba203943e0fc26e93
rayros404 <ray.ros404@gmail.com>
2022-06-21 16:52:45 -0700

    internship skeleton

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,40 @@
 import Navbar from "../components/Navbar/Navbar"
 import styles from "../styles/Home.module.css"
+import Image from "next/image"
 
 const Home = () => {
   return (
     <div id={styles.home}>
+      <section id={styles.internship}>
+      <span id={styles.internshipTitle}>Internship</span>
+      <span id={styles.internshipSubtitle}>BruinShack</span>
+        <div id={styles.internshipContent}>
+          <div id={styles.internshipText}>
+            <div id={styles.description}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit in recusandae repudiandae tempore necessitatibus, vel sapiente illum dolorum officia atque!
+              <br/>
+              <br/>
+              Technologies - HTML | SCSS | JavaScript | React | NextJS | Node | Express | Git
+            </div>
+            <a
+              id={styles.bruinshackLink}
+              href="https://bruinshack-my-contribution.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+            Show Me
+            </a>
+          </div>
+          <div id={styles.internshipImage}>
+            <Image 
+              src="/BruinShackScreenshots/homescreen.jpg"
+              height={450}
+              width={450}
+              alt="internshipHomepage"
+            />
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
